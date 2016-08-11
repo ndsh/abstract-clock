1bc551cb1b0ff4eebaa147f58249928bbdce0c19
ndsh <hej@julian-h.de>
2016-08-11 20:29:03 +0200

    more modulos

--- abstractColorClock/abstractColorClock.ino
+++ abstractColorClock/abstractColorClock.ino
@@ -390,10 +390,10 @@ void RTCoutput(){
   #endif
 }
 byte decToBcd(byte val) {
-  return ((val/10*16) + (val%10));
+  return ((val/10*16) + (val - 10 * (val / 10)));
 }
 byte bcdToDec(byte val) {
-  return ((val/16*10) + (val%16));
+  return ((val/16*10) + (val - 16 * (val / 16)));
 }
 byte wireRead() {
   #if defined (__AVR_ATtiny85__)
