c4a4261b725e8126c922ecb25c103f40bde64691
ndsh <me@julian-h.de>
2020-05-17 14:44:22 +0200

    update

---
+++ Drafts/FSM/FSM.ino
@@ -0,0 +1,25 @@
+#define INTRO 1
+#define RUN 2
+
+int state = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  stateMachine();
+}
+
+void stateMachine() {
+  switch state(state) {
+    case INTRO:
+      
+    break;
+
+    case RUN:
+    break;
+  }
+}
