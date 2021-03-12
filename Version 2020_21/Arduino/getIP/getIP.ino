52044fcd7ced8a2b454c0166773adcd588edb4f3
ndsh <me@julian-h.de>
2021-03-12 23:48:49 +0100

    getIP

---
+++ Version 2020_21/Arduino/getIP/getIP.ino
@@ -0,0 +1,66 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+HTTPClient sender;
+
+// WLAN-Daten
+const char* ssid = "SSID";
+const char* password = "PASS";
+
+
+void setup() {
+  Serial.begin(115200);
+  
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(200);
+    Serial.print(".");
+  }
+
+  Serial.println("Verbunden!");
+
+
+  if (sender.begin("http://julian-h.de/ip.php")) {
+
+    // HTTP-Code der Response speichern
+    int httpCode = sender.GET();
+   
+
+    if (httpCode > 0) {
+      
+      // Anfrage wurde gesendet und Server hat geantwortet
+      // Info: Der HTTP-Code für 'OK' ist 200
+      if (httpCode == HTTP_CODE_OK) {
+
+        // Hier wurden die Daten vom Server empfangen
+
+        // String vom Webseiteninhalt speichern
+        String payload = sender.getString();
+
+        // Hier kann mit dem Wert weitergearbeitet werden
+       // ist aber nicht unbedingt notwendig
+        Serial.println(payload);
+        
+        
+        
+      }
+      
+    }else{
+      // Falls HTTP-Error
+      Serial.printf("HTTP-Error: ", sender.errorToString(httpCode).c_str());
+    }
+
+    // Wenn alles abgeschlossen ist, wird die Verbindung wieder beendet
+    sender.end();
+    
+  }else {
+    Serial.printf("HTTP-Verbindung konnte nicht hergestellt werden!");
+  }
+  
+}
+
+
+void loop() {
+
+}
